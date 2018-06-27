4dcc065b24ab32763d53d302eaa08ffb66a2d388
rob <robmitchell@email.com>
2018-06-27 11:31:44 +0200

    new and create working with link from index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,21 @@
 class BooksController < ApplicationController
 
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def index
     @books = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def new
-
+    @book = Book.create
   end
 
   def create
+    Book.create(book_params)
+    redirect_to books_path
 
   end
 
@@ -27,6 +29,15 @@ class BooksController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :rating, :have_read)
+  end
+  def find_book
+    @book = Book.find(params[:id])
   end
 
 end

d83d2e8478c49aa1c0a451f0025b40d53387ee2b
rob <robmitchell@email.com>
2018-07-10 14:21:28 +0200

    authenticate user for all actions execpt index and show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+    #user must be logged in for all actions. Exceptions to be added
+  before_action :authenticate_user!
 end
